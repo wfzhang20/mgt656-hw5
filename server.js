098569c0de329e046c625c4ee2afbf28324ce5d4
wfzhang20 <freddy.zhang@yale.edu>
2015-10-09 22:10:40 +0000

    Add nickname

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response){
-    response.send('Hello MGT-656!');
+    response.send('Hello MGT-656! Does this even work?');
+});
+
+app.get('/nickname', function(request, response){
+    response.send('tough-armadillo');
 });
 
 app.listen(process.env.PORT || 4000);
